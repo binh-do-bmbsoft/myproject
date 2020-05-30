d8f9e6ae46f280bfb21aad0a7434223b4b131686
doducbinh <doducbinh1995@gmail.com>
2020-05-30 22:05:25 +0700

    Updates

---
+++ precache-manifest.42d854611822996dcbf10b8f6a5f40b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8d146d936e04a175675d4e36efe99c",
+    "url": "/myproject/index.html"
+  },
+  {
+    "revision": "b465d3784e8cc7bba614",
+    "url": "/myproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "580809f8d8efa4809cde",
+    "url": "/myproject/static/js/2.46ccae03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myproject/static/js/2.46ccae03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b465d3784e8cc7bba614",
+    "url": "/myproject/static/js/main.264ebae8.chunk.js"
+  },
+  {
+    "revision": "dcde2aa99c3ef7e6602f",
+    "url": "/myproject/static/js/runtime-main.fc5d438a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
